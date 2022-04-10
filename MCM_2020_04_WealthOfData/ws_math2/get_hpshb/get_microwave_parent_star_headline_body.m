780b1d756d11d03219f22d5e3de51fc339f81c6d
Calvin <liubaixv@yeah.net>
2022-04-10 17:28:24 +0800

    ws init

--- MCM_2020_04_WealthOfData/ws_math2/get_hpshb/get_microwave_parent_star_headline_body.m
+++
@@ -1,7 +0,0 @@
-%rmed_microwave_parent_star_headline_body = [microwave_parent microwave_star microwave_headline microwave_body];
-%rmed_microwave_parent_star_headline_body = rmdata(microwave_parent_star_headline_body, nrm);
-rmed_microwave_parent_star_headline_body5 = rmed_microwave_parent_star_headline_body(1:668, :);
-rmed_microwave_parent_star_headline_body4 = rmed_microwave_parent_star_headline_body(669:968, :);
-rmed_microwave_parent_star_headline_body3 = rmed_microwave_parent_star_headline_body(969:1102, :);
-rmed_microwave_parent_star_headline_body2 = rmed_microwave_parent_star_headline_body(1101:1214, :);
-rmed_microwave_parent_star_headline_body1 = rmed_microwave_parent_star_headline_body(1213:end, :);
--- MCM_2020_04_WealthOfData/ws_math2/run_assAnalysis.m
+++
@@ -1,8 +0,0 @@
-%% run_assAnalysis
-clear
-hairdryerL=1; microwaveL=2; pacifierL=3;
-i=0;
-% parfor i = 0 : 5
-disp(i);
-assAnalysis(4, 0);
-% end
